27e18ec88eaa7d43a3b65ddd74914c3e085d36cf
haoranzhang <haoranzhang001@gmail.com>
2021-04-24 07:58:45 +1000

    add responsive design -- menu toggle

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+//Responsive header
+const toggle = document.querySelector('.responsive__toggle');
+toggle.addEventListener('click', function (){
+    const body = document.querySelector('body');
+    body.classList.toggle('responsive__toggle--active');
+    //开启与关闭菜单栏。toggle后面加class 不加点。
+    this.classList.toggle('bi-x');
+    this.classList.toggle('bi-list');
+})
+
 //Introduction paragraph animation
 const typed = document.querySelector('.introduction__paragraph__animation')
 if (typed) {
