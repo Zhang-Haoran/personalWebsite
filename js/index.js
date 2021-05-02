f843c602738f5891ae2161f5e27f63e93c30cff6
haoranzhang <haoranzhang001@gmail.com>
2021-05-02 19:01:21 +1000

    add blog

--- js/index.js
+++ js/index.js
@@ -1,22 +1,44 @@
-//GET blogs data from backend
-const blogsElement = document.querySelector('.blog__list');
 const url = `http://localhost:3001`;
-async function getBlogs(){
+//POST blog data into backend
+function addBlog(){
+    const blogsTitle = document.querySelector('#title').value;
+    const blogContent = document.querySelector('#content').value;
     try {
-        return axios.get(`${url}/blogs`);
+        axios.post(`${url}/blogs/add?title=${blogsTitle}&content=${blogContent}`).then((result)=>{
+            if (result.data === 'success'){
+                alert("Add Blog successfully");
+            }
+        });
     }catch (e){
         console.log(e);
     }
 }
-getBlogs().then((result)=>{
-    result.data.map((blog)=>{
-        blogsElement.innerHTML += `<li>${blog.title}</li>`
-    })
-})
+//GET blogs data from backend
+function getBlogs() {
+    const blogsElement = document.querySelector('.blog__list');
+    try {
+        axios.get(`${url}/blogs`).then((result) => {
+            result.data.map((blog) => {
+                blogsElement.innerHTML +=
+                    `<li class="blog__list__item">
+                <h3 class="blog__list__item__title">${blog.title}</h3> 
+                <p class="blog__list__item__content">${blog.content}</p>
+                <p class="blog__list__item__created-time">Created at: ${blog.created_time}</p>
+                <p class="blog__list__item__updated-time">Last updated: ${blog.updated_time}</p>
+                <button class="btn btn-primary">Update</button>
+                <button class="btn btn-danger">Delete</button>
+             </li>`
+            })
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+getBlogs();
 
 //Responsive header
 const toggle = document.querySelector('.responsive__toggle');
-toggle.addEventListener('click', function (){
+toggle.addEventListener('click', function () {
     const body = document.querySelector('body');
     body.classList.toggle('responsive__toggle--active');
     //开启与关闭菜单栏。toggle后面加class 不需要加点。
