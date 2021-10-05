4c88d7ef3c7d8d69a5ddba66e5bfe5cd41c22020
Zhang-Haoran <haoranzhang001@gmail.com>
2021-10-05 20:22:45 +1100

    updated

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 //Url connect to backend
-const url = `https://13.59.0.242:443/api/v1`;
+const url = `http://13.59.0.242:80/api/v1`;
 //Get previous content, put it into modal form and handle user click submit button
 function updateBlogModal(id, title, content){
     let blogsTitle = document.querySelector('#updateModalTitle');
